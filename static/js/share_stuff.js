145d7c716b7deaa5cc1663752b37f663ccd1a9ea
Pat Martinez <patmartinezmtn@gmail.com>
2014-11-24 10:14:45 -0800

    Add confirm delete popup, made the javascript that makes it harder for webcrawlers to harvest email more unobtrusive.

--- static/js/share_stuff.js
+++ static/js/share_stuff.js
@@ -1,9 +1,9 @@
 
-// Script to make it harder for webcrawlers to harvest email. For example, <a href="mailto:foo...example(dot)com">foo ... example dot com</a> will become
-//     <a href="mailto:foo@example.com">foo@example.com</a>
+// Script to make it harder for webcrawlers to harvest email. For example, <a href="mailto:foo(at)gmail(dot)com">foo at gmail dot com</a>
+// will become <a href="mailto:foo@gmail.com">foo@gmail.com</a>
 $(function() {
 $('a[href^="mailto:"]').each(function() {
-this.href = this.href.replace('...', '@').replace(/\(dot\)/g, '.');
+this.href = this.href.replace('\(at\)', '@').replace(/\(dot\)/g, '.');
 // Remove this line if you don't want to set the email address as link text:
 this.innerHTML = this.href.replace('mailto:', '');
 });
@@ -18,6 +18,31 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-55827110-1', 'auto');
 ga('send', 'pageview');
+
+
+//****** Didn't use this option because was not an Unobtrusive solution ******
+// Popup box to confirm deletion of an item. For example, 
+// in template set button value to "{{ item.id }}"
+//
+// function confirmDelete(value) {
+//   if (confirm("OK to Delete?")) {
+//     var item_id = value;
+//     parent.location= "/sharing/inventory/delete_item/" + item_id;
+//   } 
+// } 
+
+
+// Popup box to confirm deletion of an item.
+$(function(){
+  $('.delete').click(function(){
+    if (confirm("OK to Delete?")){
+      parent.location= ($(this).attr('href'));
+    }
+    else {
+      return false;
+    }
+  });
+});
 
 
 // Some test code that I'm playing around with.
